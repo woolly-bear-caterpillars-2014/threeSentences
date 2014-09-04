e19d73fa7b5d8121f4bd647376dfcee2c3b0afd1
Alexei Pesic <aly.pesic@gmail.com>
2014-09-03 22:25:04 -0400

    Associations for sentence, story, user models

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,2 +1,5 @@
 class Sentence < ActiveRecord::Base
+  has_many :children, class_name: "Sentence", foreign_key: "parent_id"
+
+  belongs_to :parent, class_name: "Sentence"
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,2 +1,4 @@
 class Story < ActiveRecord::Base
+  belongs_to :user
+  has_many :sentences
 end
