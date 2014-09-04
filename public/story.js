34dc01cf8111922b0573813a6f85c1b47b040274
Alexei Pesic & David Bragdon <pair+apesic.postmodem@devbootcamp.com>
2014-09-04 15:19:50 -0400

    Rewrote sentence rendering to use flat data structure

--- public/story.js
+++ public/story.js
@@ -8,15 +8,10 @@ _.templateSettings = {
   interpolate: /\{\{(.+?)\}\}/g
 };
 
-
-_.templateSettings = {
-  interpolate: /\{\{(.+?)\}\}/g
-};
-
 var getStory = function() {
   return $.ajax({
     method: 'get',
-    url: '/example_story.json',
+    url: '/test.json',
     dataType: 'json'
   })
 }
@@ -31,6 +26,18 @@ var findSlice = function(sentence) {
   var pane = findPane(sentence);
   return pane.children('.slice[data-parent-id=' + sentence.parent_id + ']');
 }
+
+// var renderSentence = function(sentence) {
+//   console.log(sentence);
+//   if (sentence.depth === 0){
+//     var slice = $('.slice[data-depth=0]');
+//   } else {
+//     var pane = setupPane(sentence);
+//     var slice = setupSlice(sentence);
+//   }
+
+//   slice.children('p[data-position=' + sentence.position + ']').html(sentence.content);
+// }
 
 var renderSentence = function(sentence) {
   console.log(sentence);
@@ -71,59 +78,21 @@ var setupPane = function(sentence) {
   return findPane(sentence);
 }
 
-function recursiveRenderSentence(sentence) {
-  renderSentence(sentence);
+// function recursiveRenderSentence(sentence) {
+//   renderSentence(sentence);
 
-  if (sentence.children.length !== 0) {
-    sentence.children.forEach(recursiveRenderSentence);
-  } else {
-    return;
-  }
-}
-
-
-var renderPane = function(sentence) {
-  var pane = paneTemplate({depth: sentence.depth});
-  $('.content').append(pane);
-  return $(pane);
-}
-
-var renderSlice = function(sentence) {
-  var pane = findPane(sentence);
-  var slice = sliceTemplate({parent_id: sentence.parent_id, depth: sentence.depth })
-  pane.append(slice);
-}
-
-var setupSlice = function(sentence) {
-  var slice;
-  if (findSlice(sentence).length === 0) {
-    renderSlice(sentence);
-  }
-  return findSlice(sentence);
-}
-
-var setupPane = function(sentence) {
-  var pane;
-  if (findPane(sentence).length === 0) {
-    renderPane(sentence);
-  }
-  return findPane(sentence);
-}
-
-function recursiveRenderSentence(sentence) {
-  renderSentence(sentence);
-
-  if (sentence.children.length !== 0) {
-    sentence.children.forEach(recursiveRenderSentence);
-  } else {
-    return;
-  }
-}
+//   if (sentence.children.length !== 0) {
+//     sentence.children.forEach(recursiveRenderSentence);
+//   } else {
+//     return;
+//   }
+// }
 
 var renderStory = function(response) {
   response.success(function(data) {
     story = data;
-    story.sentences.forEach(recursiveRenderSentence);
+    story.sentences.forEach(renderSentence)
+    // story.sentences.forEach(recursiveRenderSentence);
   });
 }
 
