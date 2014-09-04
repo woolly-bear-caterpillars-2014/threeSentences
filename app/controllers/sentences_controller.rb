3176116fdfc6150a6f575058223f84e3d53383ca
Alexei Pesic <aly.pesic@gmail.com>
2014-09-04 17:02:15 -0400

    Basic functionality for add story, sentences

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -1,2 +1,21 @@
 class SentencesController < ApplicationController
+  def new
+
+  end
+
+  def create
+    @story = Story.find(params[:story_id])
+    @sentence = @story.sentences.build(sentence_params)
+    if @sentence.save
+      redirect_to @story
+    else
+      redirect_to @story, alert: "Error!"
+    end
+  end
+
+  private
+
+  def sentence_params
+    params.require(:sentence).permit(:depth, :position, :content, :parent_id)
+  end
 end
