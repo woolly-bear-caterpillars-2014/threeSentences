efeca80f0f3c0a9584cea01ef52bbcb16b8c84f6
Alexei Pesic <aly.pesic@gmail.com>
2014-09-03 22:17:27 -0400

    Initial migrations for stories, sentences

--- db/migrate/20140904021416_create_stories.rb
+++ db/migrate/20140904021416_create_stories.rb
@@ -1,7 +1,8 @@
 class CreateStories < ActiveRecord::Migration
   def change
     create_table :stories do |t|
-
+      t.string :name
+      t.integer :user_id
       t.timestamps
     end
   end
--- db/migrate/20140904021428_create_sentences.rb
+++ db/migrate/20140904021428_create_sentences.rb
@@ -1,6 +1,10 @@
 class CreateSentences < ActiveRecord::Migration
   def change
     create_table :sentences do |t|
+      t.integer :parent_id
+      t.integer :story_id
+      t.text :content
+      t.integer :depth
 
       t.timestamps
     end
