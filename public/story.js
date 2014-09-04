1f4cf65b868697a254ffe8344d4635d86e742b27
Alexei Pesic & David Bragdon <pair+apesic.postmodem@devbootcamp.com>
2014-09-04 10:59:47 -0400

    Add Underscore JS and begin sentence rendering
    * Set up UnderscoreJS sentenceTemplate
    * Started sentence rendering (one level)

--- public/story.js
+++ public/story.js
@@ -1,9 +1,39 @@
-$(document).ready(function() {
-  $.ajax({
+var story;
+var sentenceTemplate;
+
+var getStory = function() {
+  return $.ajax({
     method: 'get',
     url: '/example_story.json',
     dataType: 'json'
-  }).success(function(data) {
-    console.log(data);
+  })
+}
+
+
+// view
+
+var renderSentence = function(sentence) {
+  console.log(sentence);
+  $('.content').append(sentenceTemplate({sentence: sentence}));
+}
+
+var renderStory = function(response) {
+  response.success(function(data) {
+    story = data;
+    story.sentences.forEach(renderSentence);
+    // renderSentence(story.sentences[0]);
   });
+}
+
+
+// init
+
+$(document).ready(function() {
+  sentenceTemplate = _.template($('#sentence-template').html());
+
+  response = getStory();
+  renderStory(response);
 });
+
+
+
