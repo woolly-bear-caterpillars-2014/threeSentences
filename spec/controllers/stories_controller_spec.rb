76ee7a76271cb8977100c7aae6df6d9b5f5ab91a
Alexei Pesic <aly.pesic@gmail.com>
2014-09-04 17:50:11 -0400

    Set up rspec, wrote initial stories controller spec for #index

--- spec/controllers/stories_controller_spec.rb
+++ spec/controllers/stories_controller_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe StoriesController, :type => :controller do
+describe StoriesController do
+  context "GET index" do
+    context "as authenticated user" do
+      before do
+	@user = User.create(
+	  username: "Tester",
+	  email: "test@test.com",
+	  password: "password1"
+	)
+	sign_in @user
+	get :index
+      end
 
+      it "assigns all user's stories to a variable" do
+	expect(assigns(:stories)).to be
+      end
+    end
+
+    context "when unauthenticated" do
+      before do
+	sign_out :user
+	get :index
+      end
+
+      it "redirects to login" do
+	expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
 end
