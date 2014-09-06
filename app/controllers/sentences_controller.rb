4be72611542151c6cc2e3e511cd4a9314fc74973
Alexei Pesic & David Bragdon <pair+apesic.postmodem@devbootcamp.com>
2014-09-05 20:08:16 -0400

    Ajax Sentence Submission and Dynamic Page Generation
    - Submit new sentences via ajax
    - Update existing sentences via ajax
    - Bootstrap sentence json into view for existing stories
    - Render sentences into DOM from json
    - Build new sentence forms on sentence submission

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -2,7 +2,6 @@ class SentencesController < ApplicationController
   before_action :authenticate_user!
 
   def new
-
   end
 
   def create
@@ -21,6 +20,22 @@ class SentencesController < ApplicationController
     end
   end
 
+  def update
+    @story = Story.find(params[:story_id])
+
+    unless current_user == @story.user
+      return redirect_to new_user_session_path
+    end
+
+    @sentence = Sentence.find(params[:id])
+    @sentence.update(sentence_params)
+
+    if @sentence.save
+      render json: @sentence.to_json
+    else
+      render json: @sentence.errors.full_messages.to_json
+    end
+  end
 
   private
 
