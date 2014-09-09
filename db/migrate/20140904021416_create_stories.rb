fe270a673411a2dcde638dec82ed06c7b7a1b9eb
Joseph White & David Bragdon <pair+jbwhite.postmodem@devbootcamp.com>
2014-09-09 10:48:12 -0400

    Implement generation/save of share_url on Story creation

--- db/migrate/20140904021416_create_stories.rb
+++ db/migrate/20140904021416_create_stories.rb
@@ -3,7 +3,7 @@ class CreateStories < ActiveRecord::Migration
     create_table :stories do |t|
       t.string :name
       t.integer :user_id
-      t.string :share_url
+
       t.timestamps
     end
   end
