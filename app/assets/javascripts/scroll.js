a8dccd12cc6a0dbeba3a7b086fd07257c4568128
Joseph White <josephbarrywhite@gmail.com>
2014-09-09 19:32:13 -0400

    testing scrollspy

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,24 +1,103 @@
 
 $(document).ready(function() {
-        $('.column').each(function(i) {
-        var position = $(this).position();
-        console.log(position);
-        console.log('min: ' + position.top + ' / max: ' + parseInt(position.top + $(this).height()));
+        // $('.column').each(function(i) {
+        // var position = $(this).position();
+        // console.log($(this));
+        // console.log($(this).width());
 
-          $(this).scrollspy({
-            min: position.top,
-            max: position.top + $(this).height(),
+        // console.log('hi! min: ' + position.left + ' / max: ' + position.right + 'hi!' + parseInt(position.left + $(this).width()));
+
+          $('#column-0').scrollspy({
+            min: 63,
+            max: 690,
             mode: 'horizontal',
-            container: '.slidee',
+            // container: 'div .slidee',
             onEnter: function(element, position) {
               if(console) console.log('entering ' +  element.id);
-              // $("body").css('background-color', "blue");
+              $(element).css('background-color', "blue");
             },
             onLeave: function(element, position) {
               if(console) console.log('leaving ' +  element.id);
-             // $('body').css('background-color','red');
+             $(element).css('background-color','red');
             }
           });
 
+          $('#column-1').scrollspy({
+            min: 693,
+            max: 1440,
+            mode: 'horizontal',
+            // container: 'div .slidee',
+            onEnter: function(element, position) {
+              if(console) console.log('entering ' +  element.id);
+              $(element).css('background-color', "blue");
+            },
+            onLeave: function(element, position) {
+              if(console) console.log('leaving ' +  element.id);
+              $(element).css('background-color', "red");
+            }
+          });
+
+          $('#column-2').scrollspy({
+            min: 1443,
+            max: 2100,
+            mode: 'horizontal',
+            // container: 'div .slidee',
+            onEnter: function(element, position) {
+              if(console) console.log('entering ' +  element.id);
+              $(element).css('background-color', "blue");
+            },
+            onLeave: function(element, position) {
+              if(console) console.log('leaving ' +  element.id);
+              $(element).css('background-color', "red");
+            }
+          });
+
+          $('#column-3').scrollspy({
+            min: 2103,
+            max: 2900,
+            mode: 'horizontal',
+            // container: 'div .slidee',
+            onEnter: function(element, position) {
+              if(console) console.log('entering ' +  element.id);
+              $(element).css('background-color', "blue");
+            },
+            onLeave: function(element, position) {
+              if(console) console.log('leaving ' +  element.id);
+              $(element).css('background-color', "red");
+            }
+          });
+
+          $('#column-4').scrollspy({
+            min: 2903,
+            max: 3700,
+            mode: 'horizontal',
+            // container: 'div .slidee',
+            onEnter: function(element, position) {
+              if(console) console.log('entering ' +  element.id);
+              $(element).css('background-color', "blue");
+            },
+            onLeave: function(element, position) {
+              if(console) console.log('leaving ' +  element.id);
+              $(element).css('background-color', "red");
+            }
+          });
+
+          $('#the-end').scrollspy({
+            min: 3703,
+            max: 4600,
+            mode: 'horizontal',
+            // container: 'div .slidee',
+            onEnter: function(element, position) {
+              if(console) console.log('entering ' +  element.id);
+              $(element).css('background-color', "blue");
+            },
+            onLeave: function(element, position) {
+              if(console) console.log('leaving ' +  element.id);
+              $(element).css('background-color', "red");
+            }
+          });
+
+
+
+
         });
-    });
