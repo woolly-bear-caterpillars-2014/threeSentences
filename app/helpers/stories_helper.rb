dc13301259336581699aef953aedfd0701cdc228
Justin Wagner <jwag789@gmail.com>
2014-09-10 10:33:21 -0400

    Added helper to shorten title names on index

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -6,4 +6,15 @@ module StoriesHelper
       render :partial => 'stories/story_nav'
     end
   end
+
+  def shorten_title(title)
+    if title.length > 10
+      new_title = title[0..9]
+      new_title += "..."
+      return new_title
+    else
+      return title
+    end
+  end
+
 end
