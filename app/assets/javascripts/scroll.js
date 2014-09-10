5a1858ed8b7bd52dfab49a240e32d7e4f4c7345f
Joseph White <josephbarrywhite@gmail.com>
2014-09-09 20:19:45 -0400

    debug scroll

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,103 +1,104 @@
+var recordHistory = function(value){
+  history.pushState(null, null, "#" + value)
+};
+
+var goBack = function(location){
+  console.log('hi hi  hi')
+  // window.scrollTo(800,0)
+
+}
+
+
 
 $(document).ready(function() {
-        // $('.column').each(function(i) {
-        // var position = $(this).position();
-        // console.log($(this));
-        // console.log($(this).width());
 
-        // console.log('hi! min: ' + position.left + ' / max: ' + position.right + 'hi!' + parseInt(position.left + $(this).width()));
 
-          $('#column-0').scrollspy({
-            min: 63,
-            max: 690,
-            mode: 'horizontal',
-            // container: 'div .slidee',
-            onEnter: function(element, position) {
-              if(console) console.log('entering ' +  element.id);
-              $(element).css('background-color', "blue");
-            },
-            onLeave: function(element, position) {
-              if(console) console.log('leaving ' +  element.id);
-             $(element).css('background-color','red');
-            }
-          });
 
-          $('#column-1').scrollspy({
-            min: 693,
-            max: 1440,
-            mode: 'horizontal',
-            // container: 'div .slidee',
-            onEnter: function(element, position) {
-              if(console) console.log('entering ' +  element.id);
-              $(element).css('background-color', "blue");
-            },
-            onLeave: function(element, position) {
-              if(console) console.log('leaving ' +  element.id);
-              $(element).css('background-color', "red");
-            }
-          });
+  // $('#column-0').scrollspy({
+  //   min: 63,
+  //   max: 690,
+  //   mode: 'horizontal',
+  //   onEnter: function(element, position) {
+  //     recordHistory(element.id);
+  //   },
+  //   onLeave: function(element, position) {
+  //    //  if(console) console.log('leaving ' +  element.id);
+  //    // $(element).css('background-color','red');
+  //   }
+  // });
 
-          $('#column-2').scrollspy({
-            min: 1443,
-            max: 2100,
-            mode: 'horizontal',
-            // container: 'div .slidee',
-            onEnter: function(element, position) {
-              if(console) console.log('entering ' +  element.id);
-              $(element).css('background-color', "blue");
-            },
-            onLeave: function(element, position) {
-              if(console) console.log('leaving ' +  element.id);
-              $(element).css('background-color', "red");
-            }
-          });
+  // $('#column-1').scrollspy({
+  //   min: 693,
+  //   max: 1440,
+  //   mode: 'horizontal',
+  //   onEnter: function(element, position) {
+  //     recordHistory(element.id);
+  //   },
+  //   onLeave: function(element, position) {
+  //     // if(console) console.log('leaving ' +  element.id);
+  //     // $(element).css('background-color', "red");
+  //   }
+  // });
 
-          $('#column-3').scrollspy({
-            min: 2103,
-            max: 2900,
-            mode: 'horizontal',
-            // container: 'div .slidee',
-            onEnter: function(element, position) {
-              if(console) console.log('entering ' +  element.id);
-              $(element).css('background-color', "blue");
-            },
-            onLeave: function(element, position) {
-              if(console) console.log('leaving ' +  element.id);
-              $(element).css('background-color', "red");
-            }
-          });
+  // $('#column-2').scrollspy({
+  //   min: 1443,
+  //   max: 2100,
+  //   mode: 'horizontal',
+  //   onEnter: function(element, position) {
+  //     recordHistory(element.id);
+  //   },
+  //   onLeave: function(element, position) {
+  //     // if(console) console.log('leaving ' +  element.id);
+  //     // $(element).css('background-color', "red");
+  //   }
+  // });
 
-          $('#column-4').scrollspy({
-            min: 2903,
-            max: 3700,
-            mode: 'horizontal',
-            // container: 'div .slidee',
-            onEnter: function(element, position) {
-              if(console) console.log('entering ' +  element.id);
-              $(element).css('background-color', "blue");
-            },
-            onLeave: function(element, position) {
-              if(console) console.log('leaving ' +  element.id);
-              $(element).css('background-color', "red");
-            }
-          });
+  // $('#column-3').scrollspy({
+  //   min: 2103,
+  //   max: 2900,
+  //   mode: 'horizontal',
+  //   onEnter: function(element, position) {
+  //     recordHistory(element.id);
+  //   },
+  //   onLeave: function(element, position) {
+  //     // if(console) console.log('leaving ' +  element.id);
+  //     // $(element).css('background-color', "red");
+  //   }
+  // });
 
-          $('#the-end').scrollspy({
-            min: 3703,
-            max: 4600,
-            mode: 'horizontal',
-            // container: 'div .slidee',
-            onEnter: function(element, position) {
-              if(console) console.log('entering ' +  element.id);
-              $(element).css('background-color', "blue");
-            },
-            onLeave: function(element, position) {
-              if(console) console.log('leaving ' +  element.id);
-              $(element).css('background-color', "red");
-            }
-          });
+  // $('#column-4').scrollspy({
+  //   min: 2903,
+  //   max: 3700,
+  //   mode: 'horizontal',
+  //   onEnter: function(element, position) {
+  //     recordHistory(element.id);
+  //  },
+  //   onLeave: function(element, position) {
+  //     // if(console) console.log('leaving ' +  element.id);
+  //     // $(element).css('background-color', "red");
+  //   }
+  // });
+
+  // $('#the-end').scrollspy({
+  //   min: 3703,
+  //   max: 4500,
+  //   mode: 'horizontal',
+  //   onEnter: function(element, position) {
+  //     recordHistory(element.id);
+  //  },
+  //   onLeave: function(element, position) {
+  //     // if(console) console.log('leaving ' +  element.id);
+  //     // $(element).css('background-color', "red");
+  //   }
+  // });
+
+  window.addEventListener("popstate", function(e) {
+    e.preventDefault();
+    console.log('hi')
+    goBack(location.hash)
+       });
 
 
 
 
-        });
+});
