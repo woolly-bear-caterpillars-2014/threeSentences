515c8243d00db9918832b095367d3cc6bf63b9e1
Alexei Pesic <aly.pesic@gmail.com>
2014-09-29 17:22:21 -0400

    Moved short title method into story model

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -6,15 +6,4 @@ module StoriesHelper
       render :partial => 'stories/story_nav'
     end
   end
-
-  def shorten_title(title)
-    if title.length > 10
-      new_title = title[0..9]
-      new_title += "..."
-      return new_title
-    else
-      return title
-    end
-  end
-
 end
